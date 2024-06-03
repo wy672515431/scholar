46bafa6e0ddfe903c43ebc6b154770b2725f7371
wangyong <672515431@qq.com>
2024-06-03 15:44:20 +0800

    init

---
+++ src/main/java/org/buaa/scholar/ScholarApplication.java
@@ -0,0 +1,13 @@
+package org.buaa.scholar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScholarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScholarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/buaa/scholar/ScholarApplicationTests.java
@@ -0,0 +1,13 @@
+package org.buaa.scholar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScholarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
